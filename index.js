898d249f141e992c4434452877c5f2b395bb6cb1
DamilolaAjayi <ajayidamey@gmail.com>
2020-03-23 19:43:42 +0100

    js task

---
+++ index.js
@@ -0,0 +1,16 @@
+// name in a variable. 
+var name = "Tomisin Ajayi";
+
+// courses in an array. 
+var courses = ["HTML", "CSS", "Javascript"];
+
+
+function even(num){
+    for ( let i = 1; i <= 200; i++) {
+
+        if (i % 2 !== 0) {
+    
+            console.log(i);
+        }
+    }
+} 
